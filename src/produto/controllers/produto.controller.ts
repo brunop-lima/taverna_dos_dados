f544de84cf5f0990a4ddc46e92a5c3dc887cfcd5
CHEKE01 <134391765+CHEKE01@users.noreply.github.com>
2025-01-20 09:18:41 -0300

    correção

--- src/produto/controllers/produto.controller.ts
+++ src/produto/controllers/produto.controller.ts
@@ -2,53 +2,86 @@ import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPip
 import { ProdutoService } from "../services/produto.service";
 import { Produto } from "../entities/produto.entity";
   
-  @Controller("/produtos")
-  export class ProdutoController {
+@Controller("/produtos")
+export class ProdutoController {
   
-    constructor(
-      private readonly produtoService: ProdutoService
-    ) {}
+  constructor(
+  private readonly produtoService: ProdutoService
+  ) {}
   
-    @Get()
-    @HttpCode(HttpStatus.OK)
-    findAll(): Promise<Produto[]> {
-      return this.produtoService.findAll();
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Produto[]> {
+    return this.produtoService.findAll();
+  }
+  
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Produto> {
+    return this.produtoService.findById(id);
+  }
+  
+  @Get('/categoria/:categoriaId')
+  @HttpCode(HttpStatus.OK)
+  findByCategoria(@Param('categoriaId', ParseIntPipe) categoriaId: number): Promise<Produto[]> {
+    return this.produtoService.findByCategoria(categoriaId);
+  }
+  
+  @Get('/fornecedor/:fornecedorId')
+  @HttpCode(HttpStatus.OK)
+  findByFornecedor(@Param('fornecedorId', ParseIntPipe) fornecedorId: number): Promise<Produto[]> {
+    return this.produtoService.findByFornecedor(fornecedorId);
+  }
+
+  @Get('/preco/crescente')
+  @HttpCode(HttpStatus.OK)
+  findByPrecoCrescente(): Promise<Produto[]> {
+    return this.produtoService.findByPrecoCrescente();
+  }
+
+  @Get('/preco/decrescente')
+  @HttpCode(HttpStatus.OK)
+  findByPrecoDecrescente(): Promise<Produto[]> {
+    return this.produtoService.findByPrecoDecrescente();
+  }
+
+  @Get('/preco/intervalo')
+  @HttpCode(HttpStatus.OK)
+  findByIntervaloPreco(
+    @Query('min') min: number, 
+    @Query('max') max: number
+  ): Promise<Produto[]> {
+    if (min === undefined || max === undefined) {
+      throw new Error("Parâmetros 'min' e 'max' são obrigatórios!");
     }
-  
-    @Get('/:id')
-    @HttpCode(HttpStatus.OK)
-    findById(@Param('id', ParseIntPipe) id: number): Promise<Produto> {
-      return this.produtoService.findById(id);
+    return this.produtoService.findByIntervadoPreco(min, max);
+  }
+
+  @Get('/titulo')
+  @HttpCode(HttpStatus.OK)
+  findByTitulo(@Query('titulo') titulo: string): Promise<Produto[]> {
+    if (!titulo) {
+      throw new Error("Parâmetro 'titulo' é obrigatório!");
     }
+    return this.produtoService.findByTitulo(titulo);
+  }
   
-    @Get('/categoria/:categoriaId')
-    @HttpCode(HttpStatus.OK)
-    findByCategoria(@Param('categoriaId', ParseIntPipe) categoriaId: number): Promise<Produto[]> {
-      return this.produtoService.findByCategoria(categoriaId);
-    }
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() produto: Produto): Promise<Produto> {
+    return this.produtoService.create(produto);
+  }
   
-    @Get('/fornecedor/:fornecedorId')
-    @HttpCode(HttpStatus.OK)
-    findByFornecedor(@Param('fornecedorId', ParseIntPipe) fornecedorId: number): Promise<Produto[]> {
-      return this.produtoService.findByFornecedor(fornecedorId);
-    }
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() produto: Produto): Promise<Produto> {
+    return this.produtoService.update(produto);
+  }
   
-    @Post()
-    @HttpCode(HttpStatus.CREATED)
-    create(@Body() produto: Produto): Promise<Produto> {
-      return this.produtoService.create(produto);
-    }
-  
-    @Put()
-    @HttpCode(HttpStatus.OK)
-    update(@Body() produto: Produto): Promise<Produto> {
-      return this.produtoService.update(produto);
-    }
-  
-    @Delete('/:id')
-    @HttpCode(HttpStatus.NO_CONTENT)
-    delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
-      return this.produtoService.delete(id).then(() => undefined);
-    }
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.produtoService.delete(id).then(() => undefined);
+  }
   }
   
--- src/produto/services/produto.service.ts
+++ src/produto/services/produto.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { DeleteResult, Repository } from "typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 import { Produto } from "../entities/produto.entity";
 import { CategoriaService } from "../../categoria/services/categoria.service";
 
@@ -55,14 +55,7 @@ export class ProdutoService {
 
     async findByFornecedor(fornecedorId: number): Promise<Produto[]> {
       const produtos = await this.produtoRepository.find({
-        where: {
-          estoques: {
-            fornecedor: { id: fornecedorId },
-          },
-        },
-        relations: {
-          estoques: true,
-        },
+        where: {estoques: {fornecedor: { id: fornecedorId }},},relations: {estoques: true,},
       });
   
       if (!produtos.length) {
@@ -72,15 +65,57 @@ export class ProdutoService {
       return produtos;
     }
 
+    async findByPrecoCrescente(): Promise<Produto[]> {
+      return await this.produtoRepository.find({
+        order: { preco: "ASC" },
+        relations: {categoria: true, estoques: true},
+      });
+    }
+    
+    async findByPrecoDecrescente(): Promise<Produto[]> {
+      return await this.produtoRepository.find({
+        order: { preco: "DESC" },
+        relations: {categoria: true, estoques: true},
+      });
+    }
+    
+    async findByIntervadoPreco(min: number, max: number): Promise<Produto[]> {
+      const produtos = await this.produtoRepository.find({
+        where: {
+          preco: {
+            $gte: min,
+            $lte: max,
+          },
+        },
+        relations: {categoria: true, estoques: true},
+      });
+    
+      if (!produtos.length) {
+        throw new HttpException("Nenhum produto encontrado dentro do intervalo especificado!",HttpStatus.NOT_FOUND,);
+      }
+    
+      return produtos;
+    }
+    
+    async findByTitulo(titulo: string): Promise<Produto[]> {
+      const produtos = await this.produtoRepository.find({
+        where: { titulo: ILike(`%${titulo}$%`)},
+        relations: {categoria: true, estoques: true },
+      });
+    
+      if (!produtos.length) {
+        throw new HttpException("Nenhum produto encontrado com o título especificado!", HttpStatus.NOT_FOUND,);
+      }
+    
+      return produtos;
+    }
+    
  
   async create(produto: Produto): Promise<Produto> {
     if (!produto.categoria || !produto.categoria.id) {
       throw new HttpException("Categoria do produto deve ser especificada!", HttpStatus.BAD_REQUEST,);
     }
-
-    
     await this.categoriaService.findById(produto.categoria.id);
-
     return await this.produtoRepository.save(produto);
   }
 
@@ -90,15 +125,11 @@ export class ProdutoService {
     if (produto.categoria && produto.categoria.id) {
       await this.categoriaService.findById(produto.categoria.id);
     }
-
     return await this.produtoRepository.save(produto);
   }
-
    async delete(id: number): Promise<DeleteResult> {
           const item_pedido = await this.findById(id);
-          if (!item_pedido) {
-            throw new HttpException("Item não encontrado!", HttpStatus.NOT_FOUND);
-          }
+          if (!item_pedido) {throw new HttpException("Item não encontrado!", HttpStatus.NOT_FOUND);}
           return await this.produtoRepository.delete(id);
         }
 
